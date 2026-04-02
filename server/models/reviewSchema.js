80378f06d5c60678d0cfae5be30572eb8211b346
atm-sakayet-hossain <mdshakhawat434@gmail.com>
2026-04-03 01:51:42 +0600

    review schema

---
+++ server/models/reviewSchema.js
@@ -0,0 +1,16 @@
+const { default: mongoose } = require("mongoose");
+
+const reviewSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true, index: true },
+    rating: { type: Number, required: true, min: 1, max: 5 },
+    comment: { type: String, default: "", trim: true },
+    isApproved: { type: Boolean, default: false, index: true },
+    approvedAt: { type: Date, default: null },
+    approvedBy: { type: mongoose.Schema.Types.ObjectId, ref: "User", default: null },
+    createdAt: { type: Date, default: Date.now },
+  },
+  { _id: true }
+);
+
+module.exports = reviewSchema
