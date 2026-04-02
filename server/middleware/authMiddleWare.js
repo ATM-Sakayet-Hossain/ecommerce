82bd5d65006c7086586a7774ca1a56ee6855fe1a
atm-sakayet-hossain <mdshakhawat434@gmail.com>
2026-04-03 01:47:18 +0600

    middleware

--- server/middleware/authMiddleWare.js
+++ server/middleware/authMiddleWare.js
@@ -1,21 +1,20 @@
 const { VerifiedToken } = require("../services/helper");
-const { responseHandler } = require("../Utils/responseHandler");
+const { responseHandler } = require("../utils/responseHandler");
 
 const authMiddleWare = async (req, res, next) => {
     try {
         const token = req.cookies;
         if (!token["X-AS-Token"]) {
-            return responseHandler(res, "Invalid Request", 400)
+            return responseHandler.error(res, 400, "Invalid Request")
         }
         const decoded = VerifiedToken(token["X-AS-Token"])
         if (!decoded) {
-            return responseHandler(res, "Invalid Request", 400)
+            return responseHandler.error(res, 400, "Invalid Request")
         }
         req.user = decoded;
         next()
     } catch (error) {
-        console.log(error);
-        return responseHandler(res,"Invalid Request", 500)
+        responseHandler.error(res, 500, "Invalid Request")
     }
 }
 
