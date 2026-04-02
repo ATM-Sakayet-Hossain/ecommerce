bf69b712436b03f8bbeda529802d03d5fe3677ec
atm-sakayet-hossain <mdshakhawat434@gmail.com>
2026-04-03 01:25:11 +0600

    auth

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -11,18 +11,25 @@ const {
   getprofile,
   updateUserProfile,
   refreshAccessToken,
+  logout,
+  changePassword,
 } = require("../controllers/authController");
 const authMiddleWare = require("../middleware/authMiddleWare");
 const route = express.Router();
 
 route.post("/registration", registration);
-route.post("/verification", verification);
+route.post("/verifyOTP", verification);
 route.post("/resendOTP", resendOTP);
 route.post("/login", login);
+route.post("/logout", authMiddleWare, logout)
 route.post("/forgetPass", forgetPass);
 route.post("/resetPass", resetPassword);
+route.post("/changePassword", authMiddleWare, changePassword)
 route.get("/getprofile", authMiddleWare, getprofile);
 route.put("/updateUserProfile", authMiddleWare, upload.single("avatar"), updateUserProfile);
 route.post("/refreshToken", refreshAccessToken)
+route.get("/getAuthStatus", authMiddleWare, getAuthStatus)
+route.post("/deactivateAccount", authMiddleWare, deactivateAccount)
+route.put("/userStatus", authMiddleWare, roleCheckMiddleware("admin"), userStatus)
 
 module.exports = route;
